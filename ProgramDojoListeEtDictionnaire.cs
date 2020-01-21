2a78898bdd244cf2b98fc817c8ff8b5008a6337d
JeanBarthelemy <58735903+JeanBarthelemy@users.noreply.github.com>
2020-01-21 12:30:47 +0100

    added renamed program.cs in order to avoid the erasement of the program.

---
+++ ProgramDojoListeEtDictionnaire.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DojoListeEtDictionnaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integersList = new List<int> { 1, 2, 3, 4, 76, 89, 32, 4565, 789, 43, 765 };
+            Console.WriteLine(GetSumList(integersList)); 
+            Console.WriteLine(CalculateMean(integersList));
+            SortList(integersList);
+            //foreach (int el in integersList)
+                //{
+                //    Console.WriteLine(el);
+                //}
+            Console.WriteLine(integersList);
+
+
+        }
+
+        public static int GetSumList(List<int> integersList)
+        {
+            
+            int sum = 0;
+            foreach (int el in integersList)
+            {
+                sum = sum + el;
+            }
+            return sum; 
+        }
+
+        public static double CalculateMean(List<int> integersList)
+        {
+            double mean = GetSumList(integersList) / integersList.Count;
+            return mean;
+        }
+
+        public static void SortList(List<int> integersList)
+        {
+            for (int i = 0; i < integersList.Count - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < integersList.Count; j++)
+                {
+                    if (integersList[minIndex] > integersList[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp;
+                temp = integersList[i];
+                integersList[i] = integersList[minIndex];
+                integersList[minIndex] = temp;
+            }
+        }
+
+    }
+}
---
+++ ProgramDojo_Palindroms.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Palindrome_dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> palindroms = new List<string> {"pop", "maoam", "ada", "ère", "elle" };
+            string palindromString = "pop maoam ada ère elle";
+            PalindromTableFromString(palindromString);
+
+            Dictionary<string, bool> dico = PalindromTableFromString(palindromString); 
+            /*Dictionary<string, bool> palindromsOrNot = PalindromTable(palindroms);*/
+            foreach (string key  in dico.Keys)
+            {
+                Console.WriteLine("Key {0} - Value : {1}", key, dico[key]);
+            }
+
+         }
+
+        public static bool IsPalindrome(string word)
+        {
+            for(int i = 0; i < word.Length / 2; i++)
+            {
+                if(word[i] != word[(word.Length - 1 )- i ])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        public static Dictionary<string, bool> PalindromTable (List<string> palindroms)
+        {
+            Dictionary<string, bool> dico = new Dictionary<string, bool>();
+            
+            
+            for (int i = 0; i< palindroms.Count; i++)
+            {
+                string currentPalindrom = palindroms[i];
+                dico[currentPalindrom] = IsPalindrome(currentPalindrom);
+            }
+            return dico;
+        }
+
+        public static Dictionary<string, bool> PalindromTableFromString(string palindroms)
+        {
+           
+            List<string> listOfPalindroms = new List<string>(palindroms.Split(" "));
+            Dictionary<string, bool> newDico = PalindromTable(listOfPalindroms);
+            return newDico;
+
+        }
+
+
+
+    }
+
+}
